2841d4e3b53f958768a14ba8dfd429d3ba0685db
王超 <2635970493@qq.com>
2018-12-04 13:07:26 +0800

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="app">
+    <div class="pressureScreen"
+         v-if="show"
+         @click.stop.self="show = false">
+      <div class="redpack-wrap">
+        <p class="title">支持本站，欢迎领取支付宝红包。</p>
+        <img class="redpack"
+             src="/img/redpack.png"
+             alt="">
+        <a href="/img/redpack.png"
+           download="黑人视频红包.png"
+           class="save">点击保存原图</a>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "app",
-
-}
+  data: () => ({
+    show: true
+  })
+};
 </script>
 
 <style>
